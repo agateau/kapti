47d2caa1077b4e88d755ebf22652b5df9f291f80
Aurélien Gâteau <mail@agateau.com>
2015-04-09 22:25:42 +0200

    pkgcmd: use argparse

--- pkgcmd.py
+++ pkgcmd.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import sys
 
+from argparse import ArgumentParser
+
 import apt
 
 
 def main():
-    cmd = sys.argv[1]
-    name = sys.argv[2]
+    parser = ArgumentParser(description='Kapti root helper')
+    parser.add_argument('action', help='Either install or remove')
+    parser.add_argument('package', help='Name of the package')
+    args = parser.parse_args()
 
     cache = apt.Cache()
-    pkg = cache[name]
-    if cmd == 'install':
+    pkg = cache[args.package]
+    if args.action == 'install':
         pkg.mark_install()
-    elif cmd == 'remove':
+    elif args.action == 'remove':
         pkg.mark_delete()
     else:
         return 1
